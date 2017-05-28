738b07eed63e779343e4529684d8cc8ff97c6c6b
rainbowbreeze <rainbowbreeze@gmx.com>
2017-05-28 15:59:41 +0200

    stripped non useful lines in the menu

--- posteat/mainfile.py
+++ posteat/mainfile.py
@@ -4,7 +4,7 @@ from posteat import PostEat
 
 # http://www.artima.com/weblogs/viewpost.jsp?thread=4829
 def main():
-    posteat = PostEat(debug=True, persist_data=True)
+    posteat = PostEat(debug=True, persist_data=False)
     today = date.today()
     # posteat.save_menu_info_storage(today.year, today.month, today.day, 'A long meal here for today')
     menu = posteat.get_menu(today.year, today.month, today.day -1)
--- posteat/posteat.py
+++ posteat/posteat.py
@@ -22,7 +22,7 @@ class PostEat:
         """Gets the menu for the given day.
         If it's not cached on the storage, it tries to retrieve online"""
         if self.__debug:
-            print('Searching for the menu ot the day', year, month, day)
+            print('---- Searching for the menu ot the day', year, month, day)
         menu = self.__get_menu_from_storage(year, month, day)
         if menu is not None:
             return menu
@@ -34,13 +34,11 @@ class PostEat:
             # Saves menu for the next time
             if self.__persist_data:
                 if self.__debug:
-                    print('Saving data on persistent storage')
-                self.save_menu_info_storage(
-                    year, month, day,
-                    self.__strip_title_from_menu(menu))
+                    print('---- Saving data on persistent storage')
+                self.save_menu_info_storage(year, month, day, menu)
             else:
                 if self.__debug:
-                    print('Skipping save data on persistent storage')
+                    print('---- Skipping save data on persistent storage')
 
         return menu
 
@@ -62,7 +60,7 @@ class PostEat:
         menu = Query()
         result = self.__db.search(menu.menu_date == str_date)
         if self.__debug:
-            print('Result from storage: ', result)
+            print('---- Result from storage: ', result)
         if result is None or len(result) == 0:
             return None
         else:
@@ -87,13 +85,13 @@ class PostEat:
             fields='posts.limit(10)'
         )
         if self.__debug:
-            print('Read from facebook')
+            print('---- Read from facebook')
         # return posteat_posts['posts']['data'][0]['message']
 
         message = None
         for post in posteat_posts['posts']['data']:
             if self.__debug:
-                print('--------------- Post ---------------')
+                print('---- Post')
                 print(post)
 
             # Converts the string in a valid datetime object, following Facebook date format
@@ -103,12 +101,13 @@ class PostEat:
             if self.__check_if_post_has_valid_date(created_time, year, month, day):
                 # The post date is correct, now let's check for the content
                 if self.__debug:
-                    print("Foud a post for the given date")
+                    print("---- Found a post for the given date")
                 message = post['message']
                 if self.__check_if_post_is_valid_menu(message):
                     if self.__debug:
-                        print('Found the menu for the given day')
+                        print('---- Found the menu for the given day')
                         print(message)
+                    message = self.__strip_title_from_menu(message)
                     break
 
         return message
@@ -116,7 +115,7 @@ class PostEat:
     def __check_if_post_has_valid_date(self, post_datetime, year, month, day):
         """Checks if the post was written in the given day of the year"""
         if self.__debug:
-            print('Comparing ', post_datetime, ' with ', year, month, day)
+            print('---- Comparing ', post_datetime, ' with ', year, month, day)
         return post_datetime.year == year and post_datetime.month == month and post_datetime.day == day
 
     def __check_if_post_is_valid_menu(self, text):
@@ -124,14 +123,13 @@ class PostEat:
         # Get first post line and clean-up it a little bit
         title = text.splitlines()[0].casefold().strip()
         if self.__debug:
-            print('Post title: ###', title, '###')
+            print('---- Post title: ###', title, '###')
         return title in ('il menu di oggi', 'il menù di oggi')
 
     def __strip_title_from_menu(self, menu):
         """Strips the first non useful lines from message"""
         # Finds first newline
-        # TODO
-        return menu
+        return menu.split("\n",2)[2];
 
 
 
